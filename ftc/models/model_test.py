4acb1ca3dd02d46fa9fa1180120140c0455df941
Seong-hun Kim <kshoon92@gmail.com>
2021-04-28 21:13:11 +0900

    Modify the model_test.py

--- ftc/models/model_test.py
+++ ftc/models/model_test.py
@@ -14,28 +14,20 @@ vel = np.zeros((3, 1))
 quat = np.vstack([1, 0, 0, 0])
 omeg = np.zeros((3, 1))
 
-system = Multicopter()
+system = Multicopter(pos=pos, vel=vel, quat=quat, omega=omeg)
 
 t = np.array([0])
-i, j = 0
+dt = 0.01
 
-while j == 0:
-    system.set_dot(t[-1], test3)
+for i in range(20):
+    dpos, dvel, dquat, domeg = system.deriv(
+        pos[:, -1:], vel[:, -1:], quat[:, -1:], omeg[:, -1:], test3)
 
-    dpos = system.pos.dot
-    dvel = system.vel.dot
-    dquat = system.quat.dot
-    domeg = system.omega.dot
-
-    t = np.append(t, 1 + t[-1])
-    pos = np.concatenate((pos, dpos + pos[:, -1].reshape(3, 1)), axis=1)
-    vel = np.concatenate((vel, dvel + vel[:, -1].reshape(3, 1)), axis=1)
-    quat = np.concatenate((quat, dquat + quat[:, -1].reshape(4, 1)), axis=1)
-    omeg = np.concatenate((omeg, domeg + omeg[:, -1].reshape(3, 1)), axis=1)
-
-    i = i+1
-    if i == 20:
-        j = 1
+    t = np.append(t, dt + t[-1])
+    pos = np.hstack((pos, dt * dpos + pos[:, -1:]))
+    vel = np.hstack((vel, dt * dvel + vel[:, -1:]))
+    quat = np.hstack((quat, dt * dquat + quat[:, -1:]))
+    omeg = np.hstack((omeg, dt * domeg + omeg[:, -1:]))
 
 fig = plt.figure()
 ax1 = fig.add_subplot(4, 1, 1)
