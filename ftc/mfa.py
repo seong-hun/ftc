ca1c06c7ae94660a983af13b32f832309982a048
Seong-hun Kim <kshoon92@gmail.com>
2023-07-18 18:47:34 +0900

    Modifty polytope determiner

--- ftc/mfa.py
+++ ftc/mfa.py
@@ -1,22 +1,19 @@
 import numpy as np
 
-import ftc
 from ftc.controllers.Flat.flat import FlatController
-from ftc.mission_determiners.polytope_determiner import PolytopeDeterminer
+from ftc.mission_determiners.polytope_determiner import Polytope, Vertices
 
 
 class MFA:
     def __init__(self, env):
         pwm_min, pwm_max = env.plant.control_limits["pwm"]
-        self.determiner = PolytopeDeterminer(
-            pwm_min * np.ones(6), pwm_max * np.ones(6), self.allocator
-        )
+        self.vertices = Vertices(pwm_min * np.ones(6), pwm_max * np.ones(6))
         self.controller = FlatController(env.plant.m, env.plant.g, env.plant.J)
 
         dx1, dx2, dx3 = env.plant.dx1, env.plant.dx2, env.plant.dx3
         dy1, dy2 = env.plant.dy1, env.plant.dy2
         c, self.c_th = 0.0338, 128  # tq / th, th / rcmds
-        B_r2f = np.array(
+        self.B_r2f = np.array(
             (
                 [-1, -1, -1, -1, -1, -1],
                 [-dy2, dy1, dy1, -dy2, -dy2, dy1],
@@ -24,21 +21,29 @@ class MFA:
                 [-c, c, -c, c, c, -c],
             )
         )
-        self.B_f2r = np.linalg.pinv(B_r2f)
 
-    def allocator(self, nu):
-        nu_f = np.vstack((-nu[0], nu[1:]))
-        th = self.B_f2r @ nu_f
-        pwms_rotor = (th / self.c_th) * 1000 + 1000
-        return pwms_rotor
+    def distribute(self, pwms_rotor):
+        nu = self.B_r2f @ (pwms_rotor - 1000) / 1000 * self.c_th
+        return nu
 
     def predict(self, tspan, lmbd, scaling_factor=1.0):
         for t in tspan:
             FM_traj = self.controller.get_control(t)
-            nu = FM_traj[2:]
-            is_in = self.determiner.determine_is_in(
-                nu, lmbd, scaling_factor=scaling_factor
-            )
-            if not is_in:
+            nu = FM_traj[2:].ravel()
+
+            vertices = self.vertices.map(
+                lambda u_min, u_max: (lmbd * u_min, lmbd * u_max)
+            ).map(lambda u_min, u_max: shrink(u_min, u_max, scaling_factor))
+
+            if not Polytope(vertices.transform(self.distribute)).contains(nu):
                 return False
+
         return True
+
+
+def shrink(u_min, u_max, scaling_factor=1.0):
+    mean = (u_min + u_max) / 2
+    width = (u_max - u_min) / 2
+    u_min = mean - scaling_factor * width
+    u_max = mean + scaling_factor * width
+    return u_min, u_max
